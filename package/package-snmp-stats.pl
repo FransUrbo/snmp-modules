a79d4c621bd36d8a2192ae1a991e5c063f75598d
Turbo Fredriksson <turbo@bayour.com>
2014-09-17 22:56:21 +0200

    Rewrite retreival of packages etc. Still don't work - no point. It takes forever to retreive the information about 6k+ packages!

--- package/package-snmp-stats.pl
+++ package/package-snmp-stats.pl
@@ -8,7 +8,7 @@
 # }}}
 
 # {{{ Config file description and location.
-# Require the file "/etc/bind/.bindsnmp" with the following
+# Require the file "/etc/dpkg/.packagesnmp" with the following
 # defines (example values shown here!):
 #
 # If the location of the config file isn't good enough for you,
@@ -22,8 +22,6 @@ my $CFG_FILE = "/etc/dpkg/.packagesnmp";
 #
 #   Required options
 #	PKG_MGR=/usr/bin/dpkg
-#	PKG_INFO="-l \*"
-#	PKG_STAT=-s
 #	PKG_INFO_HEAD_CNT=5
 # }}}
 
@@ -37,115 +35,119 @@ use BayourCOM_SNMP;
 
 $ENV{PATH} = "/bin:/usr/bin:/usr/sbin";
 
-my $OID_BASE;
-$OID_BASE = "OID_BASE"; # When debugging, it's easier to type this than the full OID
+my $OID_BASE = "OID_BASE"; # When debugging, it's easier to type this than the full OID
 if($ENV{'MIBDIRS'}) {
     # ALWAYS override this if we're running through the SNMP daemon!
     $OID_BASE = ".1.3.6.1.4.1.8767.2.5"; # .iso.org.dod.internet.private.enterprises.bayourcom.snmp.packageStats
 }
 
-my %PKG;  # Package information.
+my %PKGS;  # Package information.
 my $NO_PACKAGES = 0; # Number of packages
 
 my %functions = (# Package stats
 		 "$OID_BASE.1.1.1" => "package_stats_index",
-		 "$OID_BASE.1.1.2" => "package_stats_data",	# name
 
 		 # Package info
-		 "$OID_BASE.2.1.1" => "package_info_index",
-		 "$OID_BASE.2.1.2" => "package_info_data");	# prio
+		 "$OID_BASE.2.1.1" => "package_info_index");
 
-my @DATA_STATS = ('Package', 'Version', 'Description', 'Status');
-my @DATA_INFO  = ('Priority', 'Section', 'Maintainer',
-		  'Source', 'Depends', 'Recommends',
-		  'Suggests', 'Size', 'MD5sum');
-# }}}
-
-# {{{ OID tree
-# smidump -f tree BAYOUR-COM-MIB.txt
-# +--packageStats(5)
-#    |
-#    +--packageStatsTable(1)
-#    |  |
-#    |  +--packageStatsEntry(1) [packageIndexStats]
-#    |     |
-#    |     +-- --- CounterIndex  packageIndexStats(1)
-#    |     +-- r-n DisplayString packageName(2)
-#    |     +-- r-n DisplayString packageVersion(3)
-#    |     +-- r-n DisplayString packageDesc(4)
-#    |     +-- r-n DisplayString packageStatus(5)
-#    |
-#    +--packageInfoTable(2)
-#       |
-#       +--packageInfoEntry(1) [packageIndexInfo]
-#          |
-#          +-- --- CounterIndex  packageIndexInfo(1)
-#          +-- r-n DisplayString packagePriority(2)
-#          +-- r-n DisplayString packageSection(3)
-#          +-- r-n DisplayString packageMaintainer(4)
-#          +-- r-n DisplayString packageSource(5)
-#          +-- r-n DisplayString packageDepends(6)
-#          +-- r-n DisplayString packageRecommends(7)
-#          +-- r-n DisplayString packageSuggests(8)
-#          +-- r-n DisplayString packageSize(9)
-#          +-- r-n DisplayString packageMD5Sum(10)
+my @DATA_STATS = ('package', 'version', 'description', 'status');
+my @DATA_INFO  = ('priority', 'section', 'maintainer',
+		  'source', 'depends', 'recommends',
+		  'suggests', 'size', 'md5sum');
 # }}}
 
 # ====================================================
 # =====    R E T R E I V E  F U N C T I O N S    =====
 
+# {{{ Get package information
+sub get_package_info {
+    my($pkgname) = shift;
+    my(%pkgs, $nr, $key);
+
+    open(STAT, "$CFG{'PKG_MGR'} -s $pkgname 2> /dev/null |")
+	|| die("Can't get package status for $pkgname, $!\n");
+
+    my $stat_line = <STAT>;
+    if(!$stat_line) {
+	$pkgs{'package'} = $pkgname;
+	$pkgs{'status'} = "!installed";
+	
+	return(%pkgs);
+    }
+    
+    do {
+	chomp($stat_line);
+	
+	if(($stat_line =~ /^[a-zA-Z]/) and ($stat_line !~ /^Conffiles:/i)) {
+	    my ($key, $value) = split(": ", $stat_line);
+	    $key = lc($key);
+	    
+	    if($key eq "status") {
+		$value =  $stat_line;
+		$value =~ s/.* //;
+	    } else {
+		$value =  (split(': ', $stat_line))[1];
+	    }
+	    
+	    if($key eq "depends") {
+		my @depends = split(', ', $value);
+		for(my $i = 0; $depends[$i]; $i++) {
+		    $pkgs{$key}{$i} = $depends[$i];
+		}
+	    } elsif($key eq "maintainer") {
+		my @maints = split(' \<', $value);
+		
+		$pkgs{$key}{'name'} = $maints[0];
+		$pkgs{$key}{'email'} = $maints[1];
+		$pkgs{$key}{'email'} =~ s/\>$//;
+	    } else {
+		$pkgs{$key} = $value;
+	    }
+	}
+	
+	$stat_line = <STAT>;
+    } while(! eof(STAT));
+
+    close(STAT);
+
+    return(%pkgs);
+}
+# }}}
+
 # {{{ Get all packages and their info
 sub get_packages {
-    my($i, $line, $nr);
+    my %pkgs;
+    my $pkgs = 1;
 
-    debug(0, "CMD: '".$CFG{'PKG_LIST'}."'\n") if($CFG{'DEBUG'} >= 4);
-    if($CFG{'PKG_LIST'} =~ /\ /) {
-	open(PKG, "$CFG{'PKG_LIST'} |")
-	    || die("Can't get a list of packages, $!\n");
-    } else {
-	open(PKG, $CFG{'PKG_LIST'})
-	    || die("Can't get a list of packages, $!\n");
+    debug(0, "CMD: ".$CFG{'PKG_MGR'}."-l \\*\n") if($CFG{'DEBUG'} >= 4);
+    open(PKG, "$CFG{'PKG_MGR'} -l \\* |")
+	|| die("Can't get a list of packages, $!\n");
+
+    # Get the header (not interested!)
+    for(my $i=0; $i < 5; $i++) {
+	my $line = <PKG>;
     }
 
     while(! eof(PKG)) {
-	$line = <PKG>; chomp($line);
+	my $list_line = <PKG>; chomp($list_line);
+	
+	my @tmp = split(' ', $list_line);
+	my $pkgname = $tmp[1];
 
-	if(($line =~ /^[a-zA-Z]/) && ($line !~ /^Conffiles:/i)) {
-	    my ($key, $value) = split(": ", $line);
-	    $key =~ s/: //;
+	my $nr = sprintf("%06d", $pkgs);
 
-	    if($key eq "Package") {
-		$NO_PACKAGES++;
+	%{$pkgs{$nr}} = &get_package_info($pkgname);
+	
+	debug(0, "=>   Package: $pkgs{$nr}{'package'} ($pkgs{$nr}{'status'})\n") if($CFG{'DEBUG'} >= 4);
 
-		$nr = sprintf("%06d", $NO_PACKAGES);
-		debug(0, "=> $nr:\n") if($CFG{'DEBUG'} >= 4);
+	last if($pkgs >= 50); # FOR DEBUGGING
 
-		open(STAT, "$CFG{'PKG_STAT'} |")
-		    || die("Can't get package status, $!\n");
-
-		# Get the header (not interested!)
-		for(my $i=0; $i < $CFG{'PKG_STAT_HEAD'}; $i++) {
-		    $line = <STAT>;
-		}
-
-		# Get the status line
-		# Desired => Unknown/Install/Remove/Purge/Hold
-		# Status  => Not/Installed/Config-files/Unpacked/Failed-config/Half-installed
-		# Err     ?= (none)/Hold/Reinst-required/X=both-problems (Status,Err: uppercase=bad)
-		$line = <STAT>; chomp($line);
-		$PKG{$nr}{'Status'} = (split(' ', $line))[0];
-
-		debug(0, "=>   Status: ".$PKG{$nr}{'Status'}."\n") if($CFG{'DEBUG'} >= 4);
-
-		close(STAT);
-	    }
-
-	    debug(0, "=>   $key: $value\n") if($CFG{'DEBUG'} >= 4);
-	    $PKG{$nr}{$key} = $value;
-	}
+	$pkgs++;
     }
 
     close(PKG);
+
+    return($pkgs, %pkgs);
 }
 # }}}
 
@@ -154,19 +156,24 @@ sub get_packages {
 
 # {{{ OID_BASE.1.1.1.x		Output basic package information - index
 sub print_package_stats_index {
-    my $dummy = shift; # This is the index, so we don't care about the package type.
+    my $nr         = shift;
     my $package_no = shift;
+
+    my($pkg_no_msg);
+    if(defined($package_no)) {
+	$pkg_no_msg = ".$package_no";
+    } else {
+	$pkg_no_msg = "";
+    }
+
     my $success = 0;
-    my $nr;
-    my $pkg_no_msg;
-    $pkg_no_msg = ".$package_no" if(defined($package_no));
 
     debug(0, "=> OID_BASE.packageStatsTable.packageStatsEntry.packageIndexStats$pkg_no_msg\n") if($CFG{'DEBUG'} > 1);
 
     if(defined($package_no)) {
 	# {{{ Specific package
 	$nr = sprintf("%06d", $package_no);
-	if(defined($PKG{$nr}{'Package'})) {
+	if(defined($PKGS{$nr}{'package'})) {
 	    debug(0, "$OID_BASE.1.1.1.$package_no = $package_no\n") if($CFG{'DEBUG'});
 
 	    debug(1, "$OID_BASE.1.1.1.$package_no\n");
@@ -178,7 +185,7 @@ sub print_package_stats_index {
 # }}}
     } else {
 	# {{{ ALL packages
-	foreach my $package_nr (sort keys %PKG) {
+	foreach my $package_nr (sort keys %PKGS) {
 	    $nr = strip_zeros($package_nr);
 
 	    debug(0, "$OID_BASE.1.1.1.$nr = $nr\n") if($CFG{'DEBUG'});
@@ -210,8 +217,8 @@ sub print_package_stats_data {
 
 	debug(0, "=> OID_BASE.packageStatsTable.packageStatsEntry.$type.$package_no\n") if($CFG{'DEBUG'} > 1);
 
-	if($PKG{$nr}{$type}) {
-	    my $package_data = $PKG{$nr}{$type};
+	if($PKGS{$nr}{$type}) {
+	    my $package_data = $PKGS{$nr}{$type};
 	    
 	    debug(0, "$OID_BASE.1.1.$package_info_type.$package_no = $package_data\n") if($CFG{'DEBUG'});
 	    
@@ -233,12 +240,20 @@ sub print_package_stats_data {
 	foreach my $type (@DATA_STATS) {
 	    debug(0, "=> OID_BASE.packageStatsTable.packageStatsEntry.$type\n") if($CFG{'DEBUG'} > 1);
 
-	    foreach my $package_nr (sort keys %PKG) {
+	    foreach my $package_nr (sort keys %PKGS) {
 		my $nr = strip_zeros($package_nr);
 
-		if($PKG{$package_nr}{$type}) {
-		    my $package_data = $PKG{$package_nr}{$type};
-		    
+		if($PKGS{$package_nr}{$type}) {
+		    my $package_data;
+
+		    if (ref($PKGS{$package_nr}{$type}) eq "HASH") {
+			for my $i (sort keys(%{$PKGS{$package_nr}{$type}})) {
+			    $package_data .= $PKGS{$package_nr}{$type}{$i};
+			}
+		    } else {
+			$package_data = $PKGS{$package_nr}{$type};
+		    }
+
 		    debug(0, "$OID_BASE.1.1.$type_counter.$nr = $package_data\n") if($CFG{'DEBUG'});
 
 		    debug(1, "$OID_BASE.1.1.$type_counter.$nr\n");
@@ -278,7 +293,7 @@ sub print_package_info_index {
     if(defined($package_no)) {
 	# {{{ Specific package
 	$nr = sprintf("%06d", $package_no);
-	if(defined($PKG{$nr}{'Package'})) {
+	if(defined($PKGS{$nr}{'Package'})) {
 	    debug(0, "$OID_BASE.2.1.1.$package_no = $package_no\n") if($CFG{'DEBUG'});
 
 	    debug(1, "$OID_BASE.2.1.1.$package_no\n");
@@ -290,7 +305,7 @@ sub print_package_info_index {
 # }}}
     } else {
 	# {{{ ALL packages
-	foreach my $package_nr (sort keys %PKG) {
+	foreach my $package_nr (sort keys %PKGS) {
 	    $nr = strip_zeros($package_nr);
 
 	    debug(0, "$OID_BASE.2.1.1.$nr = $nr\n") if($CFG{'DEBUG'});
@@ -325,8 +340,8 @@ sub print_package_info_data {
 	debug(0, "=> OID_BASE.packageInfoTable.packageInfoEntry.$type.$package_no\n") if($CFG{'DEBUG'} > 1);
 
 	debug(0, "=> CHECK: PKG{$nr}{$type}\n") if($CFG{'DEBUG'} >= 4);
-	if($PKG{$nr}{$type}) {
-	    my $package_data = $PKG{$nr}{$type};
+	if($PKGS{$nr}{$type}) {
+	    my $package_data = $PKGS{$nr}{$type};
 	    
 	    debug(0, "$OID_BASE.2.1.$package_info_type.$package_no = $package_data\n") if($CFG{'DEBUG'});
 	    
@@ -352,21 +367,36 @@ sub print_package_info_data {
 	foreach my $type (@DATA_INFO) {
 	    debug(0, "=> OID_BASE.packageInfoTable.packageInfoEntry.$type\n") if($CFG{'DEBUG'} > 1);
 
-	    foreach my $package_nr (sort keys %PKG) {
+	    foreach my $package_nr (sort keys %PKGS) {
 		my $nr = strip_zeros($package_nr);
 
-		if($PKG{$package_nr}{$type}) {
-		    my $package_data = $PKG{$package_nr}{$type};
-		    
-		    debug(0, "$OID_BASE.1.1.$type_counter.$nr = $package_data\n") if($CFG{'DEBUG'});
+		if($PKGS{$package_nr}{$type}) {
+		    my $package_data;
 
-		    debug(1, "$OID_BASE.1.1.$type_counter.$nr\n");
+		    if (ref($PKGS{$package_nr}{$type}) eq "HASH") {
+			if($type eq 'maintainer') {
+			    $package_data = $PKGS{$package_nr}{$type}{'email'};
+			} elsif($type eq 'depends') {
+			    for my $i (keys(%{$PKGS{$package_nr}{$type}})) {
+				$package_data .= $PKGS{$package_nr}{$type}{$i};
+				if($PKGS{$package_nr}{$type}{$i+1}) {
+				    $package_data .= ", ";
+				}
+			    }
+			}
+		    } else {
+			$package_data = $PKGS{$package_nr}{$type};
+		    }
+		    
+		    debug(0, "$OID_BASE.2.1.$type_counter.$nr = $package_data\n") if($CFG{'DEBUG'});
+
+		    debug(1, "$OID_BASE.2.1.$type_counter.$nr\n");
 		    debug(1, "string\n");
 		    debug(1, "$package_data\n");
 		} else {
-		    debug(0, "$OID_BASE.1.1.$type_counter.$nr = n/a\n") if($CFG{'DEBUG'});
+		    debug(0, "$OID_BASE.2.1.$type_counter.$nr = n/a\n") if($CFG{'DEBUG'});
 
-		    debug(1, "$OID_BASE.1.1.$type_counter.$nr\n");
+		    debug(1, "$OID_BASE.2.1.$type_counter.$nr\n");
 		    debug(1, "string\n");
 		    debug(1, "n/a\n");
 		}
@@ -403,27 +433,34 @@ sub strip_zeros {
 # {{{ Call function with option
 sub call_print {
     my $func_nr  = shift;
-    my $table_nr = shift;
     my $func_arg = shift;
     my $function;
-    my $nr = $table_nr;
 
-    # We (%functions) only know about 'OID_BASE.1.1.[12]' so fake it
-    $nr = 2 if($table_nr > 2);
+    # Make sure that the argument variable is initialized
+    $func_arg =  '' if(!defined($func_arg));
 
-    debug(0, "=> call_print($func_nr, $table_nr, $func_arg)\n") if($CFG{'DEBUG'} > 3);
-    my $func = $functions{$func_nr.".".$nr};
+    debug(0, "=> call_print($func_nr, $func_arg)\n") if($CFG{'DEBUG'} > 3);
+    my $func = $functions{$func_nr};
+print "=> func='$func' ($func_nr)\n";
     if($func) {
 	$function = "print_".$func;
     } else {
-	debug(0, "=> ERROR: Unknown function call_print($func_nr, $table_nr, $func_arg)\n");
+	foreach my $oid (sort keys %functions) {
+	    # Take the very first match
+	    if($oid =~ /^$func_nr/) {
+		debug(0, "=> '$oid =~ /^$func_nr/'\n") if($CFG{'DEBUG'} > 2);
+		$function = "print_".$functions{$oid};
+print "   function='$function'\n";
+		last;
+	    }
+	}
     }
 
     if(defined($function)) {
-	debug(0, "=> Calling function '$function($table_nr, $func_arg)'\n") if($CFG{'DEBUG'} > 2);
+	debug(0, "=> Calling function '$function($func_arg)'\n") if($CFG{'DEBUG'} > 2);
 	
 	$function = \&{$function}; # Because of 'use strict' above...
-	&$function($table_nr, $func_arg);
+	&$function($func_arg);
     } else {
 	return 0;
     }
@@ -434,7 +471,7 @@ sub call_print {
 # =====          P R O C E S S  A R G S          =====
 
 %CFG = get_config($CFG_FILE);
-&get_packages();
+($NO_PACKAGES, %PKGS) = &get_packages();
 
 debug(0, "=> OID_BASE => '$OID_BASE'\n") if($CFG{'DEBUG'});
 
@@ -453,6 +490,9 @@ for(my $i=0; $ARGV[$i]; $i++) {
 
 if($ALL) {
     # {{{ Output the whole MIB tree - used mainly/only for debugging purposes.
+    $functions{$OID_BASE.".1.1.2"} = "package_stats_data";
+    $functions{$OID_BASE.".2.1.2"} = "package_info_data";
+
     foreach my $oid (sort keys %functions) {
 	my $func = $functions{$oid};
 	if($func) {
@@ -462,6 +502,27 @@ if($ALL) {
     }
 # }}}
 } else {
+    # ALWAYS override the OID_BASE value since we're
+    # running through the SNMP daemon!
+    $OID_BASE = ".1.3.6.1.4.1.8767.2.5";
+
+    # {{{ Rewrite the '%functions' array (replace OID_BASE with $OID_BASE)
+    my($new_oid, %new_functions);
+    foreach my $oid (sort keys %functions) {
+	if($oid =~ /^OID_BASE/) {
+	    $new_oid =  $oid;
+	    $new_oid =~ s/OID_BASE/$OID_BASE/;
+	    
+	    $new_functions{$new_oid} = $functions{$oid};
+	}
+    }
+    %functions = %new_functions;
+# }}}
+
+    foreach my $oid (sort keys %functions) {
+	print "=> functions{$oid} = '$functions{$oid}'\n";
+    }
+
     # {{{ Go through the commands sent on STDIN
     my($arg, $oid, @tmp);
 
@@ -485,6 +546,7 @@ if($ALL) {
 	$oid =~ s/$OID_BASE//; # Remove the OID base
 	$oid =~ s/OID_BASE//;  # Remove the OID base (if we're debugging)
 	$oid =~ s/^\.//;       # Remove the first dot if it exists - it's in the way!
+	debug(0, "=> OID=$OID_BASE.$oid\n") if($CFG{'DEBUG'} > 2);
 
 	if($CFG{'DEBUG'} >= 2) {
 	    my $tmp = $OID_BASE;
@@ -530,7 +592,8 @@ if($ALL) {
 
 		# {{{ Call functions, recursively (1)
 		my $new = $OID_BASE.".".$tmp[0].".".$tmp[1];
-		debug(0, ">> Get next OID( 1): $new.".$tmp[2].".".$tmp[3]."\n") if($CFG{'DEBUG'} >= 2);
+		debug(0, ">> Get next OID( 1): $new.".$tmp[2].".".$tmp[3]."\n")
+		    if($CFG{'DEBUG'} >= 2);
 		if(!&call_print($new, $tmp[2], $tmp[3])) {
 		    # End of OID_BASE.1.1.x => goto OID_BASE.1.1.x+1.1
 		    my $max;
